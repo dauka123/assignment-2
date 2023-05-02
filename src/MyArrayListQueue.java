c124cd06da0d137b5a8e7770c2e088d3b6d2dbb4
amanzhol <rysbek_dauren@mail.ru>
2023-05-02 19:03:08 +0600

    comments added

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -2,16 +2,28 @@ public class MyArrayListQueue<T> {
     MyArrayList myArrayListQueue = new <T>MyArrayList();
 
     MyArrayListQueue() {}
+    /*
+      enqueue  - method in queue to add  an element at the end of the queue.
+    */
     public void enqueue(T element) {
         myArrayListQueue.add(element);
     }
+    /*
+      dequeue - removes and returns the front element of the queue.
+    */
     public T dequeue() {
         return (T) myArrayListQueue.remove(0);
     }
+    /*
+      peek - returns the first elements of the queue.
+    */
     public T peek() throws NoSuchFieldException {
         isEmptyThrowException();
         return (T) myArrayListQueue.get(0);
     }
+    /*
+      size - to get the size of the queue
+    */
     public int size() {
         return myArrayListQueue.size();
     }
@@ -20,6 +32,9 @@ public class MyArrayListQueue<T> {
             throw new NoSuchFieldException();
         }
     }
+    /*
+      isEmpty - to check is queue empty or not
+    */
     public boolean isEmpty() {
         return this.size() == 0;
     }
